027cecc22eb3ce081792169c1ade4e23d719c430
Abhay <rathod.abh@gmail.com>
2022-08-01 08:28:37 +0530

    updated sqlalchemy

--- sqlalchemy/main.py
+++ sqlalchemy/main.py
@@ -1,3 +1,47 @@
-from sqlalchemy import create_engine
+from ctypes import addressof
+from enum import auto
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.sql.expression import text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import logging
 
-engine = create_engine("sqlite+pysqlite:///:memory:",echo=True, future=True)
+# logging.basicConfig(
+#     format="%(asctime)s - %(name)s - %(filename)s:%(funcName)s:%(lineno)d - %(levelname)s - %(message)s")
+# log = logging.getLogger(__name__)
+# log.setLevel(logging.DEBUG)
+# ---------------use logging to improve the code
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost:5432/fastapi"
+
+Base = declarative_base()
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+session = sessionLocal()
+
+class Users(Base):
+    __tablename__ = "new_users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    email = Column(String(30))
+    address = Column(String(50), nullable=False)
+
+Base.metadata.create_all(engine)
+
+# u = [Users(name="abhay",email="rathod.abh@gmail.com",address="dfsb"),Users(name="abhay2",email="fjsdjv",address="sdbshdhsdv")]
+# session.add_all(u)
+# session.commit()
+# session.close()
+
+
+a = session.query(Users).all()
+for i in a:
+    print("name",i.name, "email",i.email, "address", i.address)
+
+b = session.query(Users).filter(Users).scalar()         # may give error if more than one field is present
+b = session.query(Users).filter(Users.id==1).scalar()      #no error kyuki filter karke sirf ek result aayega
+print(b.name)
