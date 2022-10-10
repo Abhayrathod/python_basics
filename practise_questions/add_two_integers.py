762e725c31a3ca76b97da1a7ecf7df0398a031be
Abhay <rathod.abh@gmail.com>
2022-10-10 09:32:07 +0530

    added practise questions

---
+++ practise_questions/add_two_integers.py
@@ -0,0 +1,4 @@
+a = int(input("Enter the first Integer "))      # int is neccessary otherwise it will treat it as string and the output 
+b = int(input("Enter the second number "))      # will be concatenation of both the number
+
+print("The sum is ",a + b)
---
+++ practise_questions/area_of_circle.py
@@ -0,0 +1,5 @@
+import math
+
+radius = int(input("Enter the radius "))
+area = math.pi*radius**2
+print("The area of circle is ",area)
